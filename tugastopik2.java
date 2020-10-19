fd85552d90721a10bae1b5526316ce45034ae638
Yahya-yay <72433088+Yahya-yay@users.noreply.github.com>
2020-10-20 00:28:46 +0700

    Add files via upload

---
+++ tugastopik2.java
@@ -0,0 +1,9 @@
+public class tugastopik2 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hi nama gua Yahya Mursyid");
+        System.out.println("Kali ini kita bakal coba belajar Java");
+        
+    }
+}
